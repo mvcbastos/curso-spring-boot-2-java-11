91a778298fb66e8aa7877dcbd57a75f0f65d637e
Marcus Bastos <mvcbastos@gmail.com>
2020-11-18 17:12:51 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> obj = this.repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return this.repository.save(obj);
+	}
 }
